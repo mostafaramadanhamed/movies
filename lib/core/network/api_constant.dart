4eb3566ce86fd4ea4da03ecba80d91c12f22a5cf
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-02-15 11:05:59 +0200

    add ui

--- lib/core/network/api_constant.dart
+++ lib/core/network/api_constant.dart
@@ -1,5 +1,7 @@
 class ApiConstance{
   static const  baseUrl='https://api.themoviedb.org/3';
+  static const  baseImgUrl='https://image.tmdb.org/t/p/w500';
+  static   String imgUrl(String path)=>'$baseImgUrl$path';
   static const  apiKey='f81159e5e91fdfdcfc0aa191026127aa';
   static const  movieNowPlayingPath='$baseUrl/movie/now_playing?api_key=$apiKey';
   static const  popularMoviesPath='$baseUrl/movie/popular?api_key=$apiKey';
--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -3,10 +3,13 @@ import 'package:movies/movie/data/datasource/movie_remote_datasource.dart';
 import 'package:movies/movie/data/repository/movies_repository.dart';
 import 'package:movies/movie/domain/repository/base_movies_repository.dart';
 import 'package:movies/movie/domain/usecases/get_now_playing_movies.dart';
+import 'package:movies/movie/presentation/controller/movies_bloc.dart';
 
 final sl=GetIt.instance;
 class ServicesLocator {
   void init(){
+    /// bloc
+    sl.registerFactory(() => MoviesBloc(sl()));
     /// use case
     sl.registerLazySingleton(() => GetNowPlayingMoviesUseCase(sl()));
     /// Repository
