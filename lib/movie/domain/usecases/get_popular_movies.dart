b4335a4bbd103209af14b7a1b5da96b20074ce13
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-02-18 11:32:43 +0200

    add callable class

--- lib/movie/domain/usecases/get_popular_movies.dart
+++ lib/movie/domain/usecases/get_popular_movies.dart
@@ -1,14 +1,16 @@
 import 'package:dartz/dartz.dart';
+import 'package:movies/core/useCase/base_use_case.dart';
 import 'package:movies/movie/domain/entities/movies.dart';
 import 'package:movies/movie/domain/repository/base_movies_repository.dart';
 
 import '../../../core/error/failure.dart';
 
-class GetPopularMoviesUseCase {
+class GetPopularMoviesUseCase extends BaseUseCase <List<Movie>>{
   final BaseMoviesRepository baseMoviesRepository;
 
   GetPopularMoviesUseCase(this.baseMoviesRepository);
-  Future<Either< Failure ,List<Movie>>> execute()async{
+  @override
+  Future<Either< Failure ,List<Movie>>> call()async{
     return await baseMoviesRepository.getPopularMovies();
   }
 }
--- lib/movie/presentation/controller/movies_bloc.dart
+++ lib/movie/presentation/controller/movies_bloc.dart
@@ -22,7 +22,7 @@ class MoviesBloc extends Bloc<MoviesEvent, MoviesState> {
   }
 
   FutureOr<void> _getTopRated(event, emit) async {
-    final result = await getTopRatedMoviesUseCase.execute();
+    final result = await getTopRatedMoviesUseCase();
     result.fold(
       (l) => emit(
         state.copyWith(
@@ -40,7 +40,7 @@ class MoviesBloc extends Bloc<MoviesEvent, MoviesState> {
   }
 
   FutureOr<void> _getPopularMovies(event, emit) async {
-    final result = await getPopularMoviesUseCase.execute();
+    final result = await getPopularMoviesUseCase();
     result.fold(
       (l) => emit(
         state.copyWith(
@@ -58,7 +58,7 @@ class MoviesBloc extends Bloc<MoviesEvent, MoviesState> {
   }
 
   FutureOr<void> _getNowPlayingMovies(event, emit) async {
-    final result = await getNowPlayingMoviesUseCase.execute();
+    final result = await getNowPlayingMoviesUseCase();
     result.fold(
       (l) => emit(
         state.copyWith(
