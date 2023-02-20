b7a2518a11b4dba6ffdfaaf1a2fcd1c558461ded
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-02-20 12:16:43 +0200

    add bloc of movie details

--- lib/movie/presentation/controller/details/movie_details_bloc.dart
+++ lib/movie/presentation/controller/details/movie_details_bloc.dart
@@ -1,16 +1,44 @@
 
+import 'dart:async';
+
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies/core/utils/enums.dart';
 import 'package:movies/movie/domain/entities/movie_details.dart';
+import 'package:movies/movie/domain/usecases/get_movie_details.dart';
 
 part 'movie_details_event.dart';
 part 'movie_details_state.dart';
 
 class MovieDetailsBloc extends Bloc<MovieDetailsEvent, MovieDetailsState> {
-  MovieDetailsBloc() : super(const MovieDetailsState()) {
-    on<MovieDetailsEvent>((event, emit) {
 
-    });
+
+
+  MovieDetailsBloc(this.getMovieDetailsUseCase) : super(const MovieDetailsState()) {
+    on<GetMovieDetailsEvent>(_getMovieDetailsEvent) ;
+  }
+
+  final GetMovieDetailsUseCase getMovieDetailsUseCase;
+
+  FutureOr<void> _getMovieDetailsEvent (
+      GetMovieDetailsEvent event,
+      Emitter<MovieDetailsState>
+      emit)async
+  {
+   final result=  await getMovieDetailsUseCase(MovieDetailsParams(movieId: event.id));
+
+   result.fold(
+           (l) =>emit(state.copyWith(
+             movieDetailsState: RequestState.error,
+             movieDetailsMessage: l.message,
+           ) ,),
+           (r) => emit(
+               state.copyWith(
+                 movieDetails: r,
+                 movieDetailsState: RequestState.loaded,
+               ),
+           ),
+   );
   }
 }
+
--- lib/movie/presentation/controller/details/movie_details_state.dart
+++ lib/movie/presentation/controller/details/movie_details_state.dart
@@ -3,18 +3,29 @@ part of 'movie_details_bloc.dart';
 class MovieDetailsState extends Equatable {
   final MovieDetails ? movieDetails;
   final RequestState movieDetailsState;
-  final String message;
+  final String movieDetailsMessage;
 
   const MovieDetailsState({
     this.movieDetails,
     this.movieDetailsState = RequestState.loading,
-    this.message = '',
+    this.movieDetailsMessage = '',
   });
+MovieDetailsState copyWith({
+   MovieDetails ? movieDetails,
+   RequestState ? movieDetailsState,
+   String ? movieDetailsMessage,
+}){
+  return MovieDetailsState(
+    movieDetails: movieDetails?? this.movieDetails,
+    movieDetailsState: movieDetailsState??this.movieDetailsState,
+    movieDetailsMessage: movieDetailsMessage??this.movieDetailsMessage,
+  );
+}
 
   @override
   List<Object?> get props => [
     movieDetails,
     movieDetailsState,
-    message,
+    movieDetailsMessage,
   ];
 }
