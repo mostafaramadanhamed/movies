ab62c7590ca111adba346bcb325b01d8d02283f6
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-02-20 10:41:39 +0200

    add genres model

---
+++ lib/movie/data/models/movie_details_model.dart
@@ -0,0 +1,27 @@
+import 'package:movies/movie/domain/entities/movie_details.dart';
+
+class MovieDetailsModel extends MovieDetails {
+  const MovieDetailsModel({
+    required super.backdropPath,
+    required super.id,
+    required super.overview,
+    required super.releaseDate,
+    required super.runtime,
+    required super.title,
+    required super.voteAverage,
+    required super.genres,
+  });
+
+  factory MovieDetailsModel.fromJson(Map<String, dynamic> json) {
+    return MovieDetailsModel(
+      backdropPath: json['backdrop_path'],
+      id: json['id'],
+      overview: json['overview'],
+      releaseDate: json['release_date'],
+      runtime: json['runtime'],
+      title: json['title'],
+      voteAverage: json['vote_average'],
+      genres: json['genres'],
+    );
+  }
+}
